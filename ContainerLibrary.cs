ea65740629060315a458113e04dfddda1d40b4b2
Itorius <itorius.yt@gmail.com>
2024-05-10 19:38:56 +0200

    Version 3

---
+++ ContainerLibrary.cs
@@ -0,0 +1,13 @@
+using System;
+using Terraria.ModLoader;
+
+namespace ContainerLibrary
+{
+    public class ContainerLibrary : Mod
+    {
+        public override void Load()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
